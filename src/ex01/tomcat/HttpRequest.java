828104bf84fb749908f44c607438c8ad7fbe0d05
Victor Regalado <vrrs0988@gmail.com>
2013-12-13 22:47:43 -0500

    added half of the first version of tomcat web container made by me.

---
+++ src/ex01/tomcat/HttpRequest.java
@@ -0,0 +1,21 @@
+package ex01.tomcat;
+
+import java.io.InputStream;
+
+public class HttpRequest {
+
+	public HttpRequest(InputStream input) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void parse() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public String getUri() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/ex01/tomcat/HttpResponse.java
@@ -0,0 +1,22 @@
+package ex01.tomcat;
+
+import java.io.OutputStream;
+
+public class HttpResponse {
+
+	public HttpResponse(OutputStream output) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void sendStaticResource() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void setRequest(HttpRequest request) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
