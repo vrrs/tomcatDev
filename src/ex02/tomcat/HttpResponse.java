1e4becaf9e2e31994632d618267ec6849a2a88dc
Victor Regalado <vrrs0988@gmail.com>
2013-12-17 23:38:21 -0500

    added new classes and skeleton for http server v2

---
+++ src/ex02/tomcat/HttpResponse.java
@@ -0,0 +1,18 @@
+package ex02.tomcat;
+
+import java.io.OutputStream;
+
+public class HttpResponse {
+
+	public HttpResponse(OutputStream output) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void setRequest(HttpRequest request) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+
+}
---
+++ src/ex02/tomcat/StaticResourceProcessor.java
@@ -0,0 +1,10 @@
+package ex02.tomcat;
+
+public class StaticResourceProcessor {
+
+	public void process(HttpRequest request, HttpResponse response) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
