6ee3b7a6a2c749f07da08bcdeeb1aa5ade2cb0cd
Victor Regalado <vrrs0988@gmail.com>
2013-12-19 23:59:41 -0500

    committed httpreques

--- src/ex02/tomcat/HttpRequest.java
+++ src/ex02/tomcat/HttpRequest.java
@@ -1,23 +1,49 @@
 package ex02.tomcat;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.Enumeration;
 
 public class HttpRequest {
-
-	public HttpRequest(InputStream input) {
-		// TODO Auto-generated constructor stub
+	private InputStream input; private String uri;
+	￼￼￼￼public HttpRequest(InputStream input){
+	￼this.input = input;
 	}
-
+	￼public String getUri() {
+	￼return uri;
+	￼}
+	private String parseUri(String requestString) {
+	int index1, index2;
+	index1 = requestString.indexOf(' ');
+	if (index1 != -1) {
+	index2 = requestString.indexOf(' ', index1 + 1);
+	}
+	if (index2 > index1)
+	return requestString.substring(index1 + 1, index2);
+	return null;
+	}
 	public void parse() {
-		// TODO Auto-generated method stub
+	// Read a set of characters from the socket
+	StringBuffer request = new StringBuffer(2048);
+	int i;
+	byte[] buffer = new byte[2048];
+	try {
+	i = input.read(buffer);}
+	catch (IOException e) {
+	e.printStackTrace();
+	i = -1;
+	}
+	for (int j=0; j<i; j++) {
+	}
+	request.append((char) buffer(j));
+	System.out.print(request.toString());
+	uri = parseUri(request.toString());
+	}
+	/* implementation of ServletRequest */ public Object getAttribute(String attribute) {
+	return null; }
+	public Enumeration getAttributeNames() { return null;
+	}
+	public String getRealPath(String path) {
 		
 	}
-
-	public String getUri() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	
-
 }
