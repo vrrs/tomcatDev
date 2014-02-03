44722104b546e113ed389bbe7fc08f21c98ced8d
Victor Regalado <vrrs0988@gmail.com>
2014-02-03 00:13:44 -0500

    Update StaticResourceProcessor.java

--- src/ex03/tomcat/StaticResourceProcessor.java
+++ src/ex03/tomcat/StaticResourceProcessor.java
@@ -1,6 +1,6 @@
 package ex03.tomcat;
 
-public class StaticResourceProcessor {
+public cla/ sss StaticResourceProcessor {
 
 	public void process(HttpRequest request, HttpResponse response) {
 		// TODO Auto-generated method stub
