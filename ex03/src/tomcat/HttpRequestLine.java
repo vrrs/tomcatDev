7c91288598c29e3c7b2be81e92221517a8200e9f
Victor Regalado <vrrs0988@gmail.com>
2014-03-02 23:19:45 -0500

    Added files from package to src files in ex03. Fixed pom files

---
+++ ex03/src/tomcat/HttpRequestLine.java
@@ -0,0 +1,18 @@
+package ex03.tomcat;
+
+public class HttpRequestLine {
+
+	public int methodEnd;
+	public char[] method;
+	public char[] protocol;
+	public int protocolEnd;
+	public int uriEnd;
+	public char[] uri;
+	
+	
+	public int indexOf(String string) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
